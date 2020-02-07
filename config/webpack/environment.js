e8dfad9ddcd4615155bbc3b14d55c2db794f51d0
jeferbc <jeffe.bernal@gmail.com>
2020-02-07 13:43:50 -0500

    Setup bootstrap and theme

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const erb = require('./loaders/erb')
 
+environment.loaders.prepend('erb', erb)
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
 module.exports = environment
