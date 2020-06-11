8f3150ae71af5074e1726820002d6c9015d3afba
jhon11390 <meljhon2@gmail.com>
2020-06-11 11:41:36 -0500

    Crea todos los metodos del controlador expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,49 @@
 class ExpensesController < ApplicationController
   def index
-    @tab = :expenses
+    @expenses = Expense.all
     @categories = Category.all
   end
+
+  def show
+    @expense = Expense.find(params[:id])
+end
+
+def new
+    @expense = Expense.new
+end
+
+def create
+    @expense = Expense.new(expense_params)
+    if @expense.save
+        redirect_to :expenses, notice: "El gasto fue publicado con éxito"
+    else
+        render :new
+    end
+end
+
+def edit
+    @expense = Expense.find(params[:id])
+end 
+
+def update
+    @expense = Expense.find(params[:id])
+    if @expense.update(expense_params)
+        redirect_to :expenses
+    else
+        render :edit
+    end
+end
+
+def destroy
+    @expense = Expense.find(params[:id])
+    @expense.destroy
+
+    redirect_to :expenses, notice: "El gasto fue eliminado con éxito"
+end
+
+private
+    def expense_params
+        params.require(:expense).permit(:type, :concept, :date, :amount)
+    end
+
 end
