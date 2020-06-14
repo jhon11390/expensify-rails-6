c6d1bd2eb9ce15124b289b629979e5b116f1b3c7
jhon11390 <meljhon2@gmail.com>
2020-06-14 00:12:51 -0500

    Crea partial formulario

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,48 +2,49 @@ class ExpensesController < ApplicationController
   def index
     @expenses = Expense.all
     @categories = Category.all
+    @expense = Expense.new
   end
 
   def show
     @expense = Expense.find(params[:id])
-end
+  end
 
-def new
-    @expense = Expense.new
-end
+  def new
+      @expense = Expense.new
+  end
 
-def create
-    @expense = Expense.new(expense_params)
-    if @expense.save
-        redirect_to :expenses, notice: "El gasto fue publicado con éxito"
-    else
-        render :new
-    end
-end
+  def create
+      @expense = Expense.new(expense_params)
+      if @expense.save
+          redirect_to :expenses, notice: "El gasto fue publicado con éxito"
+      else
+          render :new
+      end
+  end
 
-def edit
-    @expense = Expense.find(params[:id])
-end 
+  def edit
+      @expense = Expense.find(params[:id])
+  end 
 
-def update
-    @expense = Expense.find(params[:id])
-    if @expense.update(expense_params)
-        redirect_to :expenses
-    else
-        render :edit
-    end
-end
+  def update
+      @expense = Expense.find(params[:id])
+      if @expense.update(expense_params)
+          redirect_to :expenses
+      else
+          render :edit
+      end
+  end
 
-def destroy
-    @expense = Expense.find(params[:id])
-    @expense.destroy
+  def destroy
+      expense = Expense.find(params[:id])
+      expense.destroy
 
-    redirect_to :expenses, notice: "El gasto fue eliminado con éxito"
-end
+      redirect_to :expenses
+  end
 
-private
-    def expense_params
-        params.require(:expense).permit(:type, :concept, :date, :amount)
-    end
+  private
+      def expense_params
+          params.require(:expense).permit(:type, :concept, :date, :amount)
+      end
 
 end
